5956dbd35348b66e03a46699e3ff84e4fbe8325e
Curtis Lee <crl029@ucsd.edu>
2018-08-21 15:05:21 -0700

    added data api route

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var	express 	  = require('express'),
-    bodyParser  = require('body-parser'),
-	  app		      = express();
+    bodyParser    = require('body-parser'),
+    app		      = express();
 
 var indexRoutes = require('./routes/index');
+var apiRoutes   = require('./routes/api');
 
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 3001));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/assets'));
 app.use(bodyParser.json());
@@ -12,6 +13,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // Routes
 app.use('/', indexRoutes);
+app.use('/api', apiRoutes);
 
 // Start up server
 app.listen(app.get('port'), function() {
